8c7292d88502075deda4c77cfa168be3dc9a3850
omkar3720 <omkarshinde2728@gmail.com>
2026-01-02 21:52:47 +0530

    Initial commit

---
+++ src/main/java/com/app/SpringApp/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.app.SpringApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController
+{
+
+@RequestMapping("/")
+public String prelogin()
+{
+    return "login";
+}
+}
---
+++ src/main/webapp/JSP/login.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Login Page</title>
+</head>
+<body>
+ <h1>Hi This is Jsp Page</h1>
+</body>
+</html
